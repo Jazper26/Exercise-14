a69f9d84eaa86159dc9d0e892cf2a1e1a4f7e380
Jazper26 <jazperhaapin89Gmail.com>
2024-01-26 21:31:00 +0800

    Iterate each row within to print the stars and also adding Printing a star

--- Exercise 14.py
+++ Exercise 14.py
@@ -4,3 +4,9 @@ rows = 5
 # Iterate through each row in reverse order
 for i in range(rows, 0, -1):
     
+    # Iterate within each row to print the stars
+    for j in range(i):
+        
+        print("*", end=" ")  # Print a star
+   
+   
